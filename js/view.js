bcab54799161dac6713dbbcf6d1720939976e165
NohYujeong <shdbwjd0326@naver.com>
2021-03-01 21:07:17 +0900

    feat: add document on view

--- js/view.js
+++ js/view.js
@@ -5,6 +5,7 @@
       this.templates = window.app.Templates;
     }
 
+    // render 함수
     render(page) {
       if (page === "Board") {
         document.querySelector("#root").innerHTML = this.templates.Board(
@@ -13,18 +14,22 @@
       }
     }
 
+    // element 선택 함수
     getElement(selector) {
       return document.querySelector(selector);
     }
 
+    // elements 선택 함수
     getElements(selector) {
       return document.querySelectorAll(selector);
     }
 
+    // add event listener 함수
     addEvent(elementID, event, func) {
       document.getElementById(elementID).addEventListener(event, func);
     }
 
+    // list & item input hide/show trigger 함수
     showTitleInput(textID, inputID) {
       document.getElementById(textID).className = document
         .getElementById(textID)
